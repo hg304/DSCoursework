a3cdcf8ad5119af1b0b2c0cf25bb6a7f98055f3a
hg304 <ec19051@qmul.ac.uk>
2022-04-16 14:47:46 +0100

    getting number of servers now async function

--- ECS656U-796PLab2StartingPoint-main/grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
+++ ECS656U-796PLab2StartingPoint-main/grpc-client/src/main/java/com/example/grpc/client/grpcclient/GRPCClientService.java
@@ -3,6 +3,9 @@ package com.example.grpc.client.grpcclient;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import com.example.grpc.server.grpcserver.PingRequest;
 import com.example.grpc.server.grpcserver.PongResponse;
@@ -198,7 +201,6 @@ public class GRPCClientService {
         ManagedChannel channel8 = ManagedChannelBuilder.forAddress("10.128.0.9",9090).usePlaintext().build();
         MatrixServiceGrpc.MatrixServiceBlockingStub stub8 = MatrixServiceGrpc.newBlockingStub(channel8);
 
-
         MatrixServiceGrpc.MatrixServiceBlockingStub[] stubs = {stub1, stub2, stub3, stub4, stub5, stub6, stub7, stub8};
         List<MatrixServiceGrpc.MatrixServiceBlockingStub> selectedstubs = new ArrayList<MatrixServiceGrpc.MatrixServiceBlockingStub>();
 
@@ -223,13 +225,15 @@ public class GRPCClientService {
                 B.add(temp);
         }
 
-        int numberServers = getNumberServers(A.get(0), B.get(0), stubs[0], (matrixA.length*matrixA.length), deadline);
+        int numberServers = getNumberServers(A.get(0), B.get(0), channel1, (matrixA.length*matrixA.length), deadline);
 
         System.out.println("Number of servers to be used for operation: " + numberServers + "/8 servers");
 
         for (int i = 0; i < numberServers; i++) {
                 selectedstubs.add(stubs[i]);
         }
+
+        ExecutorService executors = Executors.newFixedThreadPool(numberServers);
 
         int[][] finalm = new int[matrixA.length][matrixA.length];
         int stubcounter = 0;
@@ -253,15 +257,24 @@ public class GRPCClientService {
         return finalm;
         }
 
-        public int getNumberServers(InnerList.Builder a, InnerList.Builder b, MatrixServiceGrpc.MatrixServiceBlockingStub stub, int amountOfCalls, long deadline) {
+        public int getNumberServers(InnerList.Builder a, InnerList.Builder b, ManagedChannel channel, int amountOfCalls, long deadline) {
                 MatrixRequest.Builder temp = MatrixRequest.newBuilder();
+                MatrixServiceGrpc.MatrixServiceStub tempstub = MatrixServiceGrpc.newStub(channel);
                 temp.setA(a);
                 temp.setB(b);
+                long end = 0;
+                ExecutorService exec = Executors.newFixedThreadPool(1);
                 long start = System.nanoTime();
+                System.out.println("Asynchronous operation being done\n")
                 System.out.println("CALCULATING NUMBER OF SERVERS NEEDED.....");
-                MatrixReply rep = stub.multiplyBlock(temp.build());
-                long end = System.nanoTime();
+                Future<MatrixReply> rep = exec.submit(tempstub.multiplyBlock(temp.build()));
+                if (rep.isDone) {
+                        end = System.nanoTime();
+                }
+                exec.shutdown();
+                System.out.println("Number of block calls: " + amountOfCalls);
                 long footprint = end - start;
+                System.out.println("Start time: " + start + " End time: " + end + " Elapsed time: " + footprint);
                 long numberOfServersLong = (footprint*amountOfCalls) / deadline;
                 int numberOfServers = Math.toIntExact(numberOfServersLong);
                 if (numberOfServers > 8) {
